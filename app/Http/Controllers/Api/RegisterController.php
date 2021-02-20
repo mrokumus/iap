5623886004809ee2505dc7c7ffadf5c2f4653011
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-02-21 01:18:11 +0300

    Controller
    Dosyalar yeniden isimlendirildi

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -5,11 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-
-class Register extends Controller
+class RegisterController extends Controller
 {
-    public function register()
-    {
-
-    }
+    //
 }
