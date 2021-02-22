63628da7d3b7355276086786f5841ff09e5cdd2a
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-02-22 05:08:37 +0300

    Update api.php
    route düzenlemesi

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 //Api Routes
 Route::post('register', [\App\Http\Controllers\Api\RegisterController::class, 'register']);
