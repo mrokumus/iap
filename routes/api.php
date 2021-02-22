1ade2661b2a430ae693b0135f3efe2e56dd920c4
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-02-22 06:33:23 +0300

    Update api.php
    Update routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 //Api Routes
-Route::post('register', [\App\Http\Controllers\Api\RegisterController::class, 'register']);
+Route::post('register/{uid}/{os}/{language}/{clientToken?}', [\App\Http\Controllers\Api\RegisterController::class, 'register']);
 Route::post('purchase', [\App\Http\Controllers\Api\PurchaseController::class, 'purchase']);
 Route::post('check', [\App\Http\Controllers\Api\CheckSubscriptionController::class, 'checkSubscription']);
 
