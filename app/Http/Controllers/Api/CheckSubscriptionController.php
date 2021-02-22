9c372f768c350d2d127eaf3081d443ec89e6c961
Muhammed Raşit OKUMUŞ <37367819+mrokumus@users.noreply.github.com>
2021-02-22 08:18:02 +0300

    Update CheckSubscriptionController.php

--- app/Http/Controllers/Api/CheckSubscriptionController.php
+++ app/Http/Controllers/Api/CheckSubscriptionController.php
@@ -2,15 +2,71 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Api\Android\AndroidController;
+use App\Http\Controllers\Api\Ios\IosController;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Api\ApplicationModel;
+use App\Models\Api\PurchaseModel;
+use App\Models\Api\RegisterModel;
 
 class CheckSubscriptionController extends Controller
 {
 
-    public function checkSubscription()
+    public function checkSubscription($clientToken)
     {
-        return response()->json('haha');
+        $device = RegisterModel::where('clientToken', '=', $clientToken)->get();
+        if (!empty($device[0])) {
 
+            //clientToken ile cihazın belirlenmesi
+            $uid = $device[0]->uid;
+            $os = $device[0]->os;
+
+            //uid ile appId belirlenmesi
+            $app = ApplicationModel::where('uid', '=', $uid)->get();
+            $purchaseId = $app[0]->appId . '1';
+
+            //appId ile birlikte purchase expireDate belirlenmesi
+            $purchase = PurchaseModel::where('purchaseId', '=', $purchaseId)->get();
+            $receipt = $purchase[0]->receipt;
+
+            //callApi metodu mock api'a receipt göndererek expireDate'i alıyoruz.
+            function callApi($os, $receipt)
+            {
+                if ($os == 1) {
+                    $androidController = new AndroidController();
+                    return $androidController->check($receipt);
+                } elseif ($os == 0) {
+                    $iosController = new iosController();
+                    return $iosController->check($receipt);
+                } else {
+                    return response()->json(['Bilinmeyen İşletim Sistemi', 400]);
+                }
+            }
+
+            $apiResponse = callApi($os, $receipt);
+
+            //Mock Api dan gelen verilerin değişlkenlere aktarılması
+            $status = $apiResponse['status'];
+            $code = $apiResponse['code'];
+            if (isset($apiResponse['expireDate'])) {
+                $expireDate = date('Y-m-d H:i:s', strtotime($apiResponse['expireDate']));
+            };
+
+            if ($status == true) {
+                $message = [
+                    'code' => 200,
+                    'expireDate' => $expireDate,
+                ];
+                return $message;
+            } else {
+                return response()->json('Bir hata oluştu lütfen daha sonra tekrar deneyin', $code);
+            }
+        } else {
+            $message = [
+                'code' => 404,
+                'message' => 'Token hatalı',
+            ];
+            return response()->json($message, 404);
+        }
     }
 }
